68aa7413d078b44830a8ec35748c4098256de6bb
Amir Abdulla <amir_abdulla@outlook.com>
2020-04-18 18:05:36 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Landing Page Project
+
+## Table of Contents
+
+* [Instructions](#instructions)
+
+## Description
+
+This is the second project in the Udacity Nanodegree Program, Front End Developer.
+In this project I was presented an HTML structure and CSS styling, with there were 3 different tasks to do.
+
+Task 1: With JS, create a navigation dynamaically as an unordered list, based on how many sections there are and sections Id and data.
+
+Task 2: Make sure that the section being viewed is highlighted in some way, such as an 'active-class'.
+
+Task 3: Clicking on an item from the navigation menu, the link should scroll to the appropiate section.
+
---
+++ js/app.js
@@ -0,0 +1,63 @@
+// Variable declarations
+const sections = document.getElementsByTagName('section');
+const navBar = document.getElementById("navbar__list");
+let liElement, dataType, idType, content, sectionId, elementToScroll;
+let anchors = document.getElementsByTagName('a');
+
+// Function to create nav menu based on amount of sections and sections info.
+function makeMenu (){
+    for (let section of sections){
+        liElement = document.createElement('li');
+        dataType = section.getAttribute('data-nav');
+        idType = section.getAttribute('id');
+        content = `<a class="menu__link" href="${idType}">${dataType}</a>`;
+        liElement.innerHTML = content;
+        navBar.appendChild(liElement);
+    };
+}
+
+// Function to scroll to section when clicking on item from nav menu.
+function scrollTo() {
+    for (let anchor of anchors) {
+        anchor.addEventListener('click', function(e){
+            e.preventDefault();
+            sectionId = anchor.getAttribute('href');
+            elementToScroll = document.getElementById(sectionId);
+            elementToScroll.scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            });
+        });
+    }
+}
+
+// Function to see if section is in viewport. 
+// If it is, turn on active-class, if not, remove active-class.
+function makeActive() {
+    for (let section of sections) { 
+        let attr = section.getAttribute('id');
+        let a = document.querySelector(`[href="${attr}"]`);
+        let box = section.getBoundingClientRect(); 
+        if (box.top <= 150 && box.bottom >= 150) { 
+            section.classList.add('your-active-class');
+            a.classList.add('menu__link__active');
+        }
+        else {
+            section.classList.remove('your-active-class');
+            a.classList.remove('menu__link__active');
+        }
+    } 
+}
+
+// Create eventlistener for when loading the page.
+window.addEventListener('load', (event) => {
+    makeMenu();    
+    scrollTo();
+});
+
+// Create eventlistener for scrolling.
+document.addEventListener('scroll', (e) => {
+    makeActive();
+});
+
+
